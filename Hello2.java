43d819fa768f314f88eebfbd46d6c051fc9cfa40
Piyaporn E <piyaporn.e@motiftech.com>
2017-08-30 19:05:10 +0700

    Hello2 Add name "Piyaporn Eiamsakul"

---
+++ Hello2.java
@@ -0,0 +1,18 @@
+
+
+class Hello {
+    
+    public Hello() {
+        System.out.println("Hello! We are:");
+        this.Krerk();
+    }
+    
+    private void Krerk() {
+        System.out.println("Trainer - Krerk Piromsopa, Ph.D.");
+        System.out.println("  Trainee  - Piyaporn Eiamsakul, Miss.");
+    }
+    
+    public static void main(String [] argv) {
+        new Hello();
+    }
+}
